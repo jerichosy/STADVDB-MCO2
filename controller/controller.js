b16d1609fc2244b929c36b18cdc92df3269bd806
jerichosy <69558553+jerichosy@users.noreply.github.com>
2023-04-14 17:31:56 +0800

    Test query

--- controller/controller.js
+++ controller/controller.js
@@ -1,8 +1,14 @@
+const node1 = require('../models/db');
 
 const controller = {
     getIndex: function (req, res) {
         res.render('index')
     },
+
+    testQuery: async function (req, res) {
+        const [rows, fields] = await node1.promise().query(`SELECT * FROM movies`);
+        res.send(rows);
+    }
 }
 
 module.exports = controller;
