1b269819fbd5465704a0f0178a43ff77e5c43423
andres_dalisay <andres_dalisay@dlsu.edu.ph>
2023-04-14 17:36:39 +0800

    passed sample data for display

--- controller/controller.js
+++ controller/controller.js
@@ -1,7 +1,39 @@
 
 const controller = {
     getIndex: function (req, res) {
-        res.render('index')
+
+        //find SELECT * FROM movies;
+        //store result in movies
+
+        movies = [
+            {
+            id: 1,
+            title: "Everything Everywhere All At Once",
+            year: 2022,
+            genre: "Sci-fi",
+            director: "Daniel Kwan",
+            actor: "Michelle Yeoh"
+            },
+            {
+                id: 2,
+                title: "The Super Mario Bros. Movie",
+                year: 2023,
+                genre: "Adventure",
+                director: "Aaron Horvath",
+                actor: "Chris Pratt"
+            },
+            {
+                id: 3,
+                title: "SP02 Ricardo Dalisay",
+                year: 2023,
+                genre: "Action",
+                director: "Matthew Sy",
+                actor: "Andres Dalisay"
+            },
+
+            
+    ]
+        res.render('index', {movies})
     },
 }
 
