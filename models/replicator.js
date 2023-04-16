37bbc289feaf2bdc6b067e9d88c1cb281bc5627b
andres_dalisay <andres_dalisay@dlsu.edu.ph>
2023-04-16 23:35:32 +0800

    finished auto replicate/sync function

--- models/replicator.js
+++ models/replicator.js
@@ -1,33 +1,19 @@
-function checkForReplicationUpdates() {
+const replicateDatabase = function replicateDatabase(syncer, arg1, arg2) {
     // Perform your replication task here, e.g., checking for updates and replicating data
-    succeeded = true
-
-    return new Promise((resolve, reject) => {
-        // Resolve or reject the promise depending on the result of the replication task
-        if (succeeded) {
-            resolve(/* Replication task result */);
-        } else {
-            reject("kase naman");
-        }
-    });
-}
-
-const replicateDatabase = function replicateDatabase() {
-    // Perform your replication task here, e.g., checking for updates and replicating data
-    checkForReplicationUpdates()
+    syncer(arg1, arg2)
         .then((result) => {
             // Handle the result
-            console.log("Replication task completed");
+            // console.log("Replication task completed");
 
             // Schedule the next replication task only when the previous task is complete
-            setTimeout(replicateDatabase, 1000);
+            setTimeout(() => replicateDatabase(syncer, arg1, arg2), 5000);
         })
         .catch((error) => {
             // Handle the error
             console.error("An error occurred during replication:", error);
 
             // Schedule the next replication task only when the previous task is complete
-            setTimeout(replicateDatabase, 1000);
+            setTimeout(() => replicateDatabase(syncer, arg1, arg2), 5000);
         });
 }
 
